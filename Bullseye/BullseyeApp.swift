e51374f9bf5b169e7a5aa6f9f88be7a300854b4c
Bruno Campos <bruno.mcamps@outlook.com>
2023-04-05 08:06:30 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Bruno campos on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
