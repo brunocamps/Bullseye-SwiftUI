715774bed7ff4e7d9643acaf4fcd7ae2bebd77d9
Bruno Campos <bruno.mcamps@outlook.com>
2023-04-18 19:36:06 +0200

    updated

---
+++ Bullseye/Views/BackgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundView.swift
+//  Bullseye
+//
+//  Created by m1 on 17/04/2023.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
+    }
+}
--- Bullseye/Views/RoundedViews.swift
+++ Bullseye/Views/RoundedViews.swift
@@ -14,7 +14,10 @@ struct RoundedViewStroked: View {
             .font(.title)
             .foregroundColor(Color("TextColor"))
             .frame(width: 56, height: 56)
-        
+            .overlay(
+                Circle()
+                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2)
+            )
     }
 }
 
@@ -24,8 +27,10 @@ struct RoundedImageViewFilled: View {
     var body: some View {
         Image(systemName: systemName)
             .font(.title)
-            .foregroundColor(Color("TextColor"))
+            .foregroundColor(Color("ButtonFilledTextColor"))
             .frame(width: 56, height: 56)
+            .background(
+                Circle().fill(Color("ButtonFilledBackgroundColor ")))
         
     }
 }
@@ -42,6 +47,6 @@ struct PreviewView: View {
 struct RoundedViews_Previews: PreviewProvider {
     static var previews: some View {
         PreviewView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.light)
     }
 }
